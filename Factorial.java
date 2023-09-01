6fab58a0175df72bb019238c3c0898c5a701c916
BoopathiSubbu <143779397+BoopathiSubbu@users.noreply.github.com>
2023-09-01 08:49:07 +0300

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Factorial {
+	  int n;
+	 long factorial=1;
+	void display()
+	{
+		/*
+		 * if(n==0) { System.out.println("The factorial is 1"); } else {
+		 */
+		for(int i=1;i<=n;i++)
+		{
+			factorial=factorial*i;
+		}
+	}
+	public static void main(String args[]) {
+		System.out.println("Enter the number");
+		Scanner s=new Scanner(System.in);
+		Factorial sss=new Factorial();
+		sss.n=s.nextInt();
+		sss.display();
+		System.out.println("The factorial is\n" +sss.factorial);
+	
+	}
+
+}
