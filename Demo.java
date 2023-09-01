2ad2ffcabe22f227e3d2e452b33af6a8ae4921d9
BoopathiSubbu <143779397+BoopathiSubbu@users.noreply.github.com>
2023-09-01 09:05:49 +0300

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,10 @@
 package com.java;
 
 public class Demo {
-int a=123;
+
 public static void main(String args[]) {
-	Demo d=new Demo();
+	int a=123;
+	//Demo d=new Demo();
 	System.out.println(d.a);
 }
 }
